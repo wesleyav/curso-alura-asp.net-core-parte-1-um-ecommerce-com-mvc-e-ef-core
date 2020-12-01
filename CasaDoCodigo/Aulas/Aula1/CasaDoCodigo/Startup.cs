24806489d102bd35284681c6b8ac163e9dc6b24a
Wesley <wesley.contato@hotmail.com>
2020-12-01 07:14:38 -0300

    Método EnsureCreated X Método Migrate

--- CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -50,7 +50,7 @@ namespace CasaDoCodigo
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
 
-            serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated();
+            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
         }
     }
 }
